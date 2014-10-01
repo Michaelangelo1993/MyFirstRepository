4f41e34a96ce96e2c1be4b225d0e60d28916a2ee
unknown <h021876c@k106-17.student.staffs.ac.uk>
2014-10-01 11:29:54 +0100

    Created player class, yolo

---
+++ TGP_Tutorial/TGP_Tutorial/Player.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace TGP_Tutorial
+{
+	public class Player
+	{
+		
+		public float positionX;
+		public float positionY;
+		public float positionZ;
+		public int score;
+		public int ammo;
+		
+		public Player ()
+		{
+			
+		}
+		
+		private void CheckCollision()
+		{
+			
+		}
+		
+		private void DeathSequence()
+		{
+			
+		}
+		
+		public void AddToInventory()
+		{
+			
+		}
+	}
+}
+
